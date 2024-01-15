b965badd0adc8d1a8cd901c9aeda4fde8e56271d
zrodevkaan <90235641+zrodevkaan@users.noreply.github.com>
2024-01-14 18:01:17 -0600

    I didnt push ANYTHING for the new release

--- plugins/dev.kaan.namenextotname/src/index.tsx
+++ plugins/dev.kaan.namenextotname/src/index.tsx
@@ -14,6 +14,8 @@ export interface SettingsType {
 
 const owo = await settings.init<SettingsType>("dev.kaan.identitytag");
 
+const ModalList: any = webpack.getByProps("ConfirmModal");
+
 const { SwitchItem } = components;
 
 const { copy } = (
@@ -22,9 +24,7 @@ const { copy } = (
   )
 ).default;
 
-const NameWithRole = await webpack.waitForModule<{ H: (something: string) => unknown }>(
-  webpack.filters.byProps("NameWithRole"),
-);
+const NameWithRole: any = webpack.getByProps("UsernameDecorationTypes");
 
 const inject = new Injector();
 
@@ -33,45 +33,55 @@ const DisplayName = ({ username, discriminator, startCopy, color }) => {
   // please make it easier on yourself with your next plugin.
   // exporting this stuff and MAKING it organized.
   const displayDiscriminator = discriminator && discriminator !== "0" ? `#${discriminator}` : "";
+
+  const handleCopy = () => {
+    const copyText = discriminator ? `@${username}${displayDiscriminator}` : `@${username}`;
+    startCopy(copyText);
+  };
+
   return (
     // Why did ppl question `text`. It does the same thing as span >:(
-    <text
-      style={{ userSelect: "none", color }}
-      onClick={() => startCopy(username)}>{` @${username}${displayDiscriminator}`}</text>
+    <ModalList.Tooltip text="Copy username">
+      {(data: any) => (
+        <text
+          {...data}
+          style={{ userSelect: "none", color, cursor: "pointer" }}
+          onClick={handleCopy}>
+          {` @${username}${displayDiscriminator}`}
+        </text>
+      )}
+    </ModalList.Tooltip>
   );
 };
 
 // I like async, lint doesn't. I dont care :3
 export async function start() {
-  inject.after(NameWithRole, "H", (a: any) => {
-    const nameHolder = a[0]?.children;
-    const className: string = a[0]?.className;
+  inject.after(NameWithRole, "default", (a: any) => {
+    const iHateTS: (tree: Record<string, unknown>) => boolean = (x) => Boolean(x?.decorations);
+    const Decorations = util.findInReactTree(a, iHateTS).decorations[1];
+    const filterFunction: (tree: Record<string, unknown>) => boolean = (x) => Boolean(x?.username);
+    const textColorMaybeOwO: (tree: Record<string, unknown>) => boolean = (x) =>
+      Boolean(x?.colorString); // albrt suggested this so sure.
 
-    if (className?.includes("header")) {
-      const childrenArray = nameHolder[2]?.props?.children?.props?.children as Array<any>;
-      const filterFunction: (tree: Record<string, unknown>) => boolean = (x) =>
-        Boolean(x?.username);
-      const textColorMaybeOwO: (tree: Record<string, unknown>) => boolean = (x) =>
-        Boolean(x?.colorString); // albrt suggested this so sure.
+    const actualUsername = util.findInTree(a, filterFunction);
+    const actualTextColor = util.findInTree(a, textColorMaybeOwO);
 
-      const actualUsername = util.findInTree(a, filterFunction);
-      const actualTextColor = util.findInTree(a, textColorMaybeOwO);
-      const updatedColorCauseUpdated = owo.get("roleColor", false)
-        ? actualTextColor?.colorString
-        : "";
-      if (actualUsername) {
-        const { username, discriminator } = actualUsername;
-        const displayName = (
-          <DisplayName
-            username={username}
-            discriminator={discriminator}
-            startCopy={startCopy}
-            color={updatedColorCauseUpdated}
-          />
-        );
+    const updatedColorCauseUpdated = owo.get("roleColor", false)
+      ? actualTextColor?.colorString
+      : "";
 
-        childrenArray?.splice(2, 0, displayName);
-      }
+    if (actualUsername) {
+      const { username, discriminator } = actualUsername;
+      const displayName = (
+        <DisplayName
+          username={username}
+          discriminator={discriminator}
+          startCopy={startCopy}
+          color={updatedColorCauseUpdated}
+        />
+      );
+
+      Decorations?.splice(2, 0, displayName);
     }
   });
 
