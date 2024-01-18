91c712dd1db035a7cf4e333c8f62a22510b36798
zrodevkaan <90235641+zrodevkaan@users.noreply.github.com>
2024-01-17 20:33:59 -0600

    no global regex ?
    ![Megamind GIF](https://tenor.com/view/megamind-gif-26070434)

--- plugins/dev.kaan.Reminders/src/comps/Settings.tsx
+++ plugins/dev.kaan.Reminders/src/comps/Settings.tsx
@@ -1,11 +1,11 @@
 import { settings } from "replugged";
-
+const Regex = new RegExp(/\d+/g);
 export const owo = await settings.init("dev.kaan.reminders");
 export function formatFilename(filename: string): string {
   const BananaSplit = filename.replace(".mp3", "").replaceAll("_", " ").replaceAll("-", " ");
   const SomeStuffHere = BananaSplit.split(" ");
   const CAPSSSSSSSSSSSSSSSS = SomeStuffHere.map(
-    (word) => word.replace(/\d/g, "").charAt(0).toUpperCase() + word.slice(1), // why does this not replace the numbers with an empty string
+    (word) => word.replaceAll(Regex, '').charAt(0).toUpperCase() + word.slice(1), // why does this not replace the numbers with an empty string
   );
   const ItLooksRightNowOwO = CAPSSSSSSSSSSSSSSSS.join(" ");
   return ItLooksRightNowOwO;
