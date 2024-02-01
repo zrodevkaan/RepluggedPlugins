8e442d7f3bf37f2b19ffdc638de73ff5673190a7
zrodevkaan <90235641+zrodevkaan@users.noreply.github.com>
2024-01-31 19:34:45 -0600

    Fixed BetterMessages Regex
    ll -> wwww
    now its
    ll -> ww

--- plugins/dev.kaan.BetterMessages/src/index.tsx
+++ plugins/dev.kaan.BetterMessages/src/index.tsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { common, Injector, settings, util, webpack } from "replugged";
-import { Button, SwitchItem, TextArea, TextInput } from "replugged/components";
-import { didUserEnableCustomAlertsOwO } from "../../dev.kaan.Reminders/src/sounds/SOwOunds";
+import {Button, SwitchItem, TextArea, TextInput} from "replugged/components";
+import {didUserEnableCustomAlertsOwO} from "../../dev.kaan.Reminders/src/sounds/SOwOunds";
+import {ContextMenuTypes} from "replugged/types";
 
 const { fluxDispatcher } = common;
-const MessageEngine: { sendMessage: () => void } = webpack.getByProps("_sendMessage");
+const MessageEngine: {sendMessage: () => void}  = webpack.getByProps("_sendMessage");
 export const owo = await settings.init("dev.kaan.bettermessages");
 const injector = new Injector();
+const MenuCheckboxItem = webpack.getByProps("MenuCheckboxItem");
 
 interface Message {
-  [1]: {};
-  content: String;
+  [1]: {}
+  content: String
 }
 
 export function start() {
@@ -21,17 +23,15 @@ export function start() {
     let rows = owo.get("rows");
     let MessageContent = util.findInReactTree(data, (x) => Boolean(x?.content)).content as String;
 
-    if (rows && owo.get("startRegex")) {
-      // did the user config let the plugin use the regex ?
+    if (rows && owo.get("startRegex")) { // did the user config let the plugin use the regex ?
       rows.forEach((row) => {
         const findValue = row.findValue; // this finds stuff
         const replaceValue = row.replaceValue; // this is the left text box
-
-        const regex = new RegExp(`[${findValue}${findValue.toUpperCase()}]`, "g");
+        const regex = new RegExp(findValue, "gi");
 
         MessageContent = MessageContent.replace(regex, (match) => {
-          return match === match.toUpperCase() ? replaceValue.toUpperCase() : replaceValue;
-          // it works? why
+          return match === match.toLocaleUpperCase() ? replaceValue.toLocaleUpperCase() : replaceValue;
+          // it works and no longer dupes :)
         });
       });
     }
@@ -69,7 +69,9 @@ export function Settings() {
       }
       return row;
     });
+
     setRows(updatedRows);
+    saveRow(updatedRows)
   };
 
   const addRow = () => {
@@ -82,8 +84,8 @@ export function Settings() {
     owo.set("rows", defaultRows);
   };
 
-  const saveRow = () => {
-    owo.set("rows", rows);
+  const saveRow = (newRows) => {
+    owo.set("rows", newRows);
   };
 
   const deleteRow = (index) => {
@@ -99,46 +101,41 @@ export function Settings() {
         children="Use Regex"
         note="Prevents plugin from replacing your messages with regex."
         {...util.useSetting(owo, "startRegex", false)}
-        value={owo.get("startRegex")}
-      />
+        value={owo.get("startRegex")}/>
       {rows.map((row, index) => (
         <>
           <div
             key={index}
-            style={{ display: "flex", justifyContent: "space-between", marginBottom: "10px" }}>
+            style={{display: "flex", justifyContent: "space-between", marginBottom: "10px"}}>
             <TextInput
               value={row.findValue}
               onChange={(e) => handleInputChange(index, e, "findValue")}
               placeholder="Find Regex"
-              style={{ width: "140%" }}
-            />
+              style={{width: "140%"}}/>
             <TextInput
               value={row.replaceValue}
               onChange={(e) => handleInputChange(index, e, "replaceValue")}
               placeholder="Replace"
-              style={{ width: "140%" }}
-            />
-            <Button
-              style={{ backgroundColor: "var(--status-danger)" }}
-              onClick={() => deleteRow(index)}>
-              Delete Row
-            </Button>
+              style={{width: "140%"}}/>
+            <Button style={{backgroundColor: "var(--status-danger)"}} onClick={() => deleteRow(index)}>Delete
+              Row</Button>
           </div>
         </>
       ))}
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        <Button onClick={saveRow} style={{ alignSelf: "center", marginRight: "10px" }}>
+      <div style={{display: "flex", justifyContent: "center"}}>
+        <Button onClick={saveRow} style={{alignSelf: "center", marginRight: "10px"}}>
           Save Rows
         </Button>
-        <Button onClick={resetSettings} style={{ alignSelf: "center", margin: "0 10px" }}>
+        <Button onClick={resetSettings} style={{alignSelf: "center", margin: "0 10px"}}>
           Reset Settings
         </Button>
-        <Button onClick={addRow} style={{ alignSelf: "center", marginLeft: "10px" }}>
+        <Button onClick={addRow} style={{alignSelf: "center", marginLeft: "10px"}}>
           Add Row
         </Button>
       </div>
     </React.Fragment>
-
+    
     // tharki is so silly :3
+    
   );
 }
