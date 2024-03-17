1068f310786efb20e0ca91ca8031bd35c15c8e9e
zrodevkaan <90235641+zrodevkaan@users.noreply.github.com>
2024-03-16 22:05:08 -0500

    Discord Recovery

---
+++ plugins/dev.kaan.Recovery/src/index.tsx
@@ -0,0 +1,53 @@
+import {Injector, settings, util, webpack} from "replugged";
+import {Button} from "replugged/components";
+import './styles.css';
+import {useState} from "react";
+const injector = new Injector();
+const owo = await settings.init("dev.kaan.recovery")
+// thank you tharki-poo ;3
+const ErrorScreen: any = await webpack.waitForModule(webpack.filters.bySource(/default\.track\((.*?)\.AnalyticEvents\.APP_CRASHED/))
+const { sizeLarge }: any = webpack.getByProps("sizeLarge");
+const { parse }: any = webpack.getByProps(["defaultRules", "parse"]);
+const { FormSwitch }: any = webpack.getByProps("FormSwitch")
+
+export function start() {
+  console.log(ErrorScreen)
+  injector.after(ErrorScreen.prototype, "render", (a: any, b, c: {state: {error: {message: String}}, setState: ({}) => void}) => {
+    console.log(a, b, c);
+    const children = b?.props?.action?.props?.children;
+    if (!children) return;
+    if (!c.state.error) return;
+    children?.push(
+      [
+        <Button
+          className={"recovery-button " + sizeLarge}
+          onClick={() => {
+            c.setState({ info: null, error: null });
+          }}
+        >
+          Recover Discord
+        </Button>,
+        <div
+          className={"recovery-parse"}
+        >
+          {parse(`\`\`\`${c.state?.error.message}\`\`\``)}
+        </div>
+      ]
+    );
+  });
+}
+
+export function stop(): void {
+  injector.uninjectAll();
+}
+export function Settings() {
+
+  return (
+    <FormSwitch
+      note={"Allows Discord to be automatically recovered on any React crash. (Possibly) uwu"}
+      {...util.useSetting(owo, "automaticRecover", false)}
+    >
+      Automatic Recover
+    </FormSwitch>
+  );
+}
