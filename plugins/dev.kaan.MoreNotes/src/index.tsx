4c8cbfdfd6e4032208ee192d9540ab04801427b3
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-04-16 22:02:31 -0500

    max height on scalable ? smh

--- plugins/dev.kaan.MoreNotes/src/index.tsx
+++ plugins/dev.kaan.MoreNotes/src/index.tsx
@@ -1,20 +1,33 @@
 import { useState } from 'react';
-import {Injector, settings, util, webpack} from 'replugged';
-import {SwitchItem, TextArea} from 'replugged/components';
+import { Injector, settings, util, webpack } from 'replugged';
+import { SwitchItem, TextArea } from 'replugged/components';
 import './styles.css';
 
 const owo = await settings.init('dev.kaan.morenotes');
 const injector = new Injector();
 const NotesArea = webpack.getBySource('action:"SET_NOTE"', { raw: true })?.exports;
 const { Heading }: any = webpack.getByProps('Heading');
-const UserProfileNotesArea: any = webpack.getBySource("UserProfileSections.USER_INFO_CONNECTIONS",{raw:true})?.exports;
+//const UserProfileNotesArea: any = webpack.getBySource("UserProfileSections.USER_INFO_CONNECTIONS", { raw: true })?.exports;
+const ProfilePopoutNotesArea: any = webpack.getBySource('.Messages.NOTE_PLACEHOLDER', { raw: true })?.exports
 
 export function start() {
   injector.after(
     NotesArea,
     'default',
     (args: any, res, instance) => {
-      addMoreNotesComponent(res.props.children, args[0].user.id);
+      addMoreNotesComponent(res.props.children, args[0].user?.id);
+    }
+  );
+  injector.after(
+    ProfilePopoutNotesArea,
+    'default',
+    (args: any, res, instance) => {
+      //console.log(args, res)
+      if (!res?.props?.children) return
+      return <div>
+        uwu
+      </div>
+      //addMoreNotesComponent(res?.props?.children, args[0].user?.id);
     }
   );
   /*injector.after(
@@ -55,23 +68,22 @@ function addMoreNotesComponent(children, userId) {
   const [text, setText] = useState(getUserNote(userId));
   const notesEnabled = owo.get('defaultNotesEnabled');
   const notesHeader = <Heading className="moreNotesHeader">MORE NOTES</Heading>;
-  const notesTextArea = 
-  <TextArea
-    placeholder="Click to add MORE notes."
-    className="moreNotes_uwu"
-    onChange={(e) => handleNoteChange(e, userId, setText)}
-    value={text}
-  />;
+  const notesTextArea =
+    <TextArea
+      placeholder="Click to add MORE notes."
+      className="moreNotes_uwu"
+      onChange={(e) => handleNoteChange(e, userId, setText)}
+      value={text}
+    />;
 
   notesEnabled ? children.splice(0, 2, notesHeader, notesTextArea) : children.push(notesHeader, notesTextArea);
 }
 
-export function Settings()
-{
-  return(
+export function Settings() {
+  return (
     <div>
       <SwitchItem note={"This allows you to remove the default note area."}
-                  {...util.useSetting(owo,'defaultNotesEnabled',false)}>
+        {...util.useSetting(owo, 'defaultNotesEnabled', false)}>
         Disable Normal Notes
       </SwitchItem>
     </div>
